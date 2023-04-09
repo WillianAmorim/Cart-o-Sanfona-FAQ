7885eb422be04fe7283175b42c06466876da40c9
Willian <willian.amorim1@outlook.com>
2023-04-09 16:10:48 -0300

    add função de rotacionar a seta alterando propriedade css

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ const tagP = document.querySelectorAll('.p-hidden')
 for (let i = 0; i < divClick.length; i++) {
   divClick[i].addEventListener('click', () => {
     const paragraph = divClick[i].nextElementSibling
-    
+
+    const arrow = divClick[i].querySelector(':nth-child(2)')
+
     if(paragraph.classList.contains('p-show')) {
       paragraph.classList.remove('p-show')
       paragraph.classList.add('p-hidden')
+      arrow.style.transform = 'rotate(0)'
+
     } else {
       paragraph.classList.remove('p-hidden')
       paragraph.classList.add('p-show')
+      arrow.style.transform = 'rotate(180deg)'
     }
   })
 }
