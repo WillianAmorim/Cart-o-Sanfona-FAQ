2ffbd75d2ebebe453490a4a685d774d7e7f129fb
Willian <willian.amorim1@outlook.com>
2023-04-09 15:59:34 -0300

    add funçao que mostra a tag p ou que oculta a tag p

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const divClick = document.querySelectorAll('.div-click')
+const tagP = document.querySelectorAll('.p-hidden')
 
+for (let i = 0; i < divClick.length; i++) {
+  divClick[i].addEventListener('click', () => {
+    const paragraph = divClick[i].nextElementSibling
+    
+    if(paragraph.classList.contains('p-show')) {
+      paragraph.classList.remove('p-show')
+      paragraph.classList.add('p-hidden')
+    } else {
+      paragraph.classList.remove('p-hidden')
+      paragraph.classList.add('p-show')
+    }
+  })
+}
