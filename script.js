fa9fc15b62d093d2e0f2ed7dcb0fb791bae7f444
Willian <willian.amorim1@outlook.com>
2023-04-10 13:53:18 -0300

    finalizado funcionalidade de aparecer e ocultar tag p

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 const divClick = document.querySelectorAll('.div-click')
-const tagP = document.querySelectorAll('.p-hidden')
 
 let lastParagraph = null
+
+console.log(lastParagraph)
 
 for (let i = 0; i < divClick.length; i++) {
   divClick[i].addEventListener('click', () => {
     const paragraph = divClick[i].nextElementSibling
 
-    const arrow = divClick[i].querySelector(':nth-child(2)')
 
-    if(lastParagraph !== paragraph && lastParagraph !== null) {
+    if(paragraph !== lastParagraph && lastParagraph !== null) {
       lastParagraph.classList.remove('p-show')
       lastParagraph.classList.add('p-hidden')
+      lastParagraph.previousElementSibling.children[1].style.transform = 'rotate(0deg)'
+      lastParagraph.previousElementSibling.children[0].style.fontWeight = '400'
+      lastParagraph.previousElementSibling.children[1].style.color = '#4B4C5F'
     }
 
-    if(paragraph.classList.contains('p-show')) {
-      paragraph.classList.remove('p-show')
-      paragraph.classList.add('p-hidden')
-      arrow.style.transform = 'rotate(0)'
-      console.log('contem')
-
-    } else {
+    if(paragraph.classList.contains('p-hidden')) {
       paragraph.classList.remove('p-hidden')
       paragraph.classList.add('p-show')
-      arrow.style.transform = 'rotate(180deg)'
-      console.log('não contem')
+      divClick[i].children[1].style.transform = 'rotate(180deg)'
+      divClick[i].children[0].style.fontWeight = '700'
+      divClick[i].children[0].style.color = '#1E1F36'
       lastParagraph = paragraph
+    } else {
+      paragraph.classList.remove('p-show')
+      paragraph.classList.add('p-hidden')
+      divClick[i].children[1].style.transform = 'rotate(0deg)'
+      divClick[i].children[0].style.fontWeight = '400'
+      divClick[i].children[0].style.color = '#4B4C5F'
     }
+
+    console.log(lastParagraph.previousElementSibling.children[0])
+    
   })
 }
